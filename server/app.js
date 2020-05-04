0e8675166e667c4f3ef428e46421f7fe30876851
Lucas Bagatini <lucasbagata@gmail.com>
2020-05-04 00:40:46 -0300

    express server

---
+++ server/app.js
@@ -0,0 +1,67 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require('mysql');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: '3357',
+  user: 'mysql',
+  password: 'mysql123',
+  database: 'mysqldb'
+});
+
+// Near the other imports
+var mysql = require('mysql');
+// Replace with your configurations
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: '3357',
+  user: 'mysql',
+  password: 'mysql123',
+  database: 'mysqldb'
+});
+connection.connect(function(error) {
+  if (error) {
+    console.error('There was a problem connecting: ' + error.stack);
+    return;
+  }
+  console.log('Connected as id ' + connection.threadId);
+});
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
